c7e3f22a15ae23b96941de581755ced1ccb00008
unknown <anggara.nc@gmail.com>
2014-11-14 01:23:56 +0700

    add button

--- protected/modules/roomA/js/view.js
+++ protected/modules/roomA/js/view.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
     
+   $("#lamp1checked").click(function() {
+        var checkbox = $('#lamp1checked');
+        if(checkbox.prop('checked') == true)
+            $("#lamp1").show();
+        else{
+            $("#lamp1").hide();
+        }
+   });
+   
+   $("#lamp2checked").click(function() {
+        var checkbox = $('#lamp2checked');
+        if(checkbox.prop('checked') == true)
+            $("#lamp2").show();
+        else{
+            $("#lamp2").hide();
+            
+        }
+   });
+        
     $("#lampA1").bootstrapSwitch();
     $("#lampA2").bootstrapSwitch();
     
@@ -32,4 +51,36 @@ $(document).ready(function() {
             });
         }
       });
+      
+      $("#lamp1Start").DateTimePicker();
+      $("#lamp1Stop").DateTimePicker();
+      $("#lamp2Start").DateTimePicker();
+      $("#lamp2Stop").DateTimePicker();
+      
+      $("#buttonLamp1").click(function(){
+           var lamp1TimerStart= $("input[name='lamp1TimerStart']").val();
+           var lamp1TimerStop= $("input[name='lamp1TimerStop']").val();
+           
+           if(lamp1TimerStart != "" && lamp1TimerStop != ""){
+                $.getJSON( webroot + '/roomA/lampA1Timer?start=' + lamp1TimerStart +'&stop='+  lamp1TimerStop, function(roomA1){
+                });
+           }
+           else{
+               alert("Please input timer");
+           }
+      });
+      
+      $("#buttonLamp2").click(function(){
+           var lamp2TimerStart= $("input[name='lamp2TimerStart']").val();
+           var lamp2TimerStop= $("input[name='lamp2TimerStop']").val();
+           if(lamp2TimerStart != "" && lamp2TimerStop != ""){
+            $.getJSON( webroot + '/roomA/lampA2Timer?start=' + lamp2TimerStart +'&stop='+  lamp2TimerStop, function(roomA1){
+            });
+           }
+           else{
+               alert("Please input timer");
+           }
+           
+      });
+          
 });
--- protected/modules/roomA/views/default/admin.php
+++ protected/modules/roomA/views/default/admin.php
@@ -25,61 +25,81 @@ Yii::app()->clientScript->registerScript('', "");
       <h3 class="panel-title">Status</h3>
     </div>
     <div class="panel-body">
-        <div class="text-center">
-            <p class="bg-primary">Lamp 1</p>
-        </div>
-        <div class="row">
-            <div class="col-md-6 col-xs-6 text-right">
-                <input type="checkbox" id="lampA1" name="my-checkbox" data-size="large" data-on-color="success" data-off-color="danger" <?php if($model->lampA1 != "on") echo "checked"; ?>>
-            </div>
-            <div class="col-md-6 col-xs-6 checkbox">
+        <div class="col-md-6 row">
+            <p class="col-md-12 text-center"><strong>LAMP 1</strong></p>
+            <input type="checkbox" id="lampA1" name="my-checkbox" data-size="large" data-on-color="success" data-off-color="danger" <?php if($model->lampA1 != "on") echo "checked"; ?>>
+            <div class="col-md-12 checkbox">
                 <label>
-                    <input type="checkbox"> Set Timer
+                    <input type="checkbox" id="lamp1checked" <?php if($model->lampA1TimerStatus != "on") echo "checked"; ?>> Set Timer
                 </label>
             </div>
-            <div class="row">
-                <div class="col-md-2 col-md-offset-4 col-xs-6 text-right">
+            <?php if($model->lampA1TimerStatus != "on"){ ?>
+            <div class="row" id="lamp1">
+                <div class="col-md-3">
                     Start : 
                     <div class="input-group">
-                        <input class="form-control" type="text" value="00:00" data-field="time">
+                        <input class="form-control" name="lamp1TimerStart" type="text" data-field="time">
                         <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-time"></span>
-                            </span>
-                        <div id="dtBox"></div>
-                        <script type="text/javascript">
-                        $("#dtBox").DateTimePicker();
-
-                        </script>
+                           <span class="glyphicon glyphicon-time"></span>
+                        </span>
+                        <div id="lamp1Start"></div>
                     </div>
                 </div>
-                <div class="col-md-2 col-xs-6">
+                <div class="col-md-3">
                     Stop :
-                    <div class="input-group clockpicker" data-placement="left">
-                        <input type="text" class="form-control" value="09:30">
+                    <div class="input-group">
+                        <input class="form-control" name="lamp1TimerStop" type="text" data-field="time">
                         <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-time"></span>
+                           <span class="glyphicon glyphicon-time"></span>
                         </span>
+                        <div id="lamp1Stop"></div>
                     </div>
-                    <script type="text/javascript">
-                    $('.clockpicker').clockpicker();
-                    </script>
+                    </br>
+                    <div class="text-center">
+                        <button type="button" id="buttonLamp1" class="btn btn-primary">Save Timer</button>
+                    </div>
+                    </br>
                 </div>
               </div>
-            
+            <?php } ?>
         </div>
-        </br>
-        <div class="text-center">
-            <p class="">Lamp 2</p>
-        </div>
-        <div>
-            <div class="col-md-6 col-xs-6 text-right">
-                <input type="checkbox" id="lampA2" name="my-checkbox" data-size="large" data-on-color="success" data-off-color="danger" <?php if($model->lampA2 != "on") echo "checked"; ?>>
-            </div>
-            <div class="col-md-6 col-xs-6 checkbox">
-                <label>
-                    <input type="checkbox"> Set Timer
-                </label>
-            </div>
+        <div class="col-md-6 row">
+            <p class="col-md-12 text-center"><strong>LAMP 2</strong></p>
+           <input type="checkbox" id="lampA2" name="my-checkbox" data-size="large" data-on-color="success" data-off-color="danger" <?php if($model->lampA2 != "on") echo "checked"; ?>>
+           <div class="col-md-12 checkbox">
+               <label>
+                   <input type="checkbox" id="lamp2checked" <?php if($model->lampA2TimerStatus != "on") echo "checked"; ?>> Set Timer
+               </label>
+           </div>
+           <?php if($model->lampA2TimerStatus != "on"){ ?>
+           <div class="row" id="lamp2">
+               <div class="col-md-3">
+                   Start : 
+                   <div class="input-group">
+                       <input class="form-control" name="lamp2TimerStart" type="text" data-field="time">
+                       <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-time"></span>
+                       </span>
+                       <div id="lamp2Start"></div>
+                   </div>
+               </div>
+               <div class="col-md-3">
+                   Stop :
+                   <div class="input-group">
+                       <input class="form-control"  name="lamp2TimerStop" type="text" data-field="time">
+                       <span class="input-group-addon">
+                          <span class="glyphicon glyphicon-time"></span>
+                       </span>
+                       <div id="lamp2Stop"></div>
+                   </div>
+                    </br>
+                    <div class="text-center">
+                        <button type="button" id="buttonLamp2" class="btn btn-primary">Save Timer</button>
+                    </div>
+                    </br>
+               </div>
+             </div>
+           <?php } ?>
         </div>
     </div>
 </div>
